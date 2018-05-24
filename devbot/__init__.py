7fd3bc6ccd91fc07676cd58546bdb60bfe5ff52d
Robin Sikkens <github@robinsikkens.nl>
2018-05-24 14:17:47 +0200

    Barebones Bot
    Doesn't actually do anything yet

---
+++ devbot/__init__.py
@@ -0,0 +1,2 @@
+''' A discord bot written to work with the DevOps server. '''
+VERSION = '0.0.1'
---
+++ devbot/bot.py
@@ -0,0 +1,29 @@
+'''
+    Main entry point for devbot.
+'''
+import logging
+import os
+import discord
+import dotenv
+from easy_logger import Logger
+
+CLIENT = discord.Client()
+''' The main discord client '''
+LOGGER = Logger().get_logger(__name__)
+
+@CLIENT.event
+async def on_ready():
+    ''' Log bot info. '''
+    LOGGER.info('Bot logged is as: %s, with id: %s.',
+            CLIENT.user.name, CLIENT.user.id)
+
+def main():
+    ''' Initialize the bot. '''
+    # Load environment variables using dotenv.
+    dotenv.load_dotenv('.env')
+
+    # Connect to discord.
+    CLIENT.run(os.environ['TOKEN'])
+
+if __name__ == '__main__':
+    main()
