ad6eb7d4187896da6fab881672ef89570cc24b7c
Robin Sikkens <github@robinsikkens.nl>
2018-05-24 22:27:59 +0200

    Feature/help (#3)
    * Help Command
    
    * Fix naming issue

---
+++ devbot/commands/help.py
@@ -0,0 +1,34 @@
+"""
+    Command module that contains help functions.
+"""
+
+from inspect import cleandoc
+from devbot.registry import Command, COMMAND_CATEGORIES
+
+
+@Command(['help', 'commands', 'list'])
+async def list_commands(*_args, **_kwargs):
+    """ Print this help message. """
+    return_list = []
+    category_list = COMMAND_CATEGORIES.keys()
+
+    for c in category_list:
+        category = c or 'No category'
+        return_list.append(f'**{category}**')
+
+        for command in COMMAND_CATEGORIES[c]:
+            name, aliases, func = command
+
+            if func.__doc__:
+                helpmessage = cleandoc(func.__doc__)  # use the function docstring as help message.
+            else:
+                helpmessage = 'You\'re on your own here.'
+
+            if aliases:
+                name = f"`!{name}` (`{'`,`'.join(aliases)}`)"
+            else:
+                name = f"`!{name}`"
+
+            return_list.append(f"{name}: {helpmessage}")
+
+    return '\n'.join(return_list)
