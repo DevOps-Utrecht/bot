55fb6cf5edd93432fb64a02d6cd26f9d4d50fbbf
Robin Sikkens <github@robinsikkens.nl>
2018-05-29 16:11:44 +0200

    Add tool to allow easily doing general API requests (#8)
    * Add tool to allow easily doing general API requests
    
    * Fix A Generator being sent back

---
+++ devbot/tools/api_requests.py
@@ -0,0 +1,39 @@
+"""
+    A tool to allow for the easy aquisition of data from RESTfull API's.
+"""
+
+import aiohttp
+
+
+async def get_json(url, *params):
+    """
+        Sends a request to a RESTfull API and returns it's JSON
+
+        use: get_json(url, (key,val), (key,val)) to include key-val pairs as params
+    """
+    parameters = [p for p in params]  # Make a list of tuples
+    async with aiohttp.get(url, params=parameters) as r:
+        if r.status == 200:
+            json = await r.json()
+            return json
+        else:
+            raise APIAccessError(f"HTTP status code {r.status} was returned")
+
+
+async def get_text(url, *params):
+    """
+        Sends a request to a RESTfull API and returns it's contents
+
+        use: get_text(url, (key,val), (key,val)) to include key-val pairs as parameters
+    """
+    parameters = [p for p in params]  # Make a list of tuples
+    async with aiohttp.get(url, params=parameters) as r:
+        if r.status == 200:
+            t = await r.text(encoding="utf-8")
+            return t
+        else:
+            raise APIAccessError(f"HTTP status code {r.status} was returned")
+
+
+class APIAccessError(Exception):
+    pass
