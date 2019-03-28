c762c4d26276048be8ae078b9551c673c727c1db
Roman Peters <mail@romanpeters.nl>
2019-03-28 21:34:56 +0100

    Add webhook-send-message-script

---
+++ scripts/say.py
@@ -0,0 +1,30 @@
+import sys
+import os
+import http.client
+import dotenv
+dotenv.load_dotenv(".env")
+
+def send( message ):
+ 
+    # your webhook URL
+    webhookurl = os.environ["WEBHOOK"]
+ 
+    # compile the form data (BOUNDARY can be anything)
+    formdata = "------:::BOUNDARY:::\r\nContent-Disposition: form-data; name=\"content\"\r\n\r\n" + message + "\r\n------:::BOUNDARY:::--"
+  
+    # get the connection and make the request
+    connection = http.client.HTTPSConnection("discordapp.com")
+    connection.request("POST", webhookurl, formdata, {
+        'content-type': "multipart/form-data; boundary=----:::BOUNDARY:::",
+        'cache-control': "no-cache",
+        })
+  
+    # get the response
+    response = connection.getresponse()
+    result = response.read()
+  
+    # return back to the calling function with the result
+    return result.decode("utf-8")
+ 
+# send the messsage and print the response
+print( send( sys.argv[1] ) )
